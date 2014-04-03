1e07e805f6f1adc2f449731ff0386d1090037307
Charles Arciniega & laura <pair+arcin.llcossette@devbootcamp.com>
2014-04-03 12:11:56 -0700

    created seeds file for tweets

--- db/migrate/20140403112542_create_tweets.rb
+++ db/migrate/20140403112542_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |col|
-      col.belongs_to :user_id
+      col.belongs_to :user
       col.string :text, :limit => 140
 
       col.timestamps
