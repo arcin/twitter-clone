45529519fcaa24a35fd5201cb95ad45e26984e68
Apprentice <apprentice@dbc15.local>
2014-04-03 10:58:09 -0700

    Add user login

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,27 +1,9 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
 end
 
-get '/bands' do
-  @band_names = Band.all.map(&:name)
-  erb :bands
+post '/profile' do
+  User.create(username: params[:username],password: params[:password])
+  erb :index
 end
 
-post '/bands' do
-  new_band = Band.create!(name: params[:name])
-  redirect "/bands/#{new_band.id}"
-end
-
-get '/bands/new' do
-  erb :new_band
-end
-
-get '/bands/:id' do
-  @band = Band.find(params[:id])
-  erb :show_band
-end
-
-get '/info' do
-  Demo.new(self).info
-end
