143c434f686ad5b75e2cac2217bcb70abe4fe16c
Apprentice <apprentice@dbc15.local>
2014-04-03 20:36:37 -0700

    Added created_at and user who wrote each tweet

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,10 @@
+helpers do
+  def find_tweets_based_off_users(users)
+    tweet_collection = []
+    users.each do |user_id|
+      user = User.find(user_id)
+      user.tweets.map {|tweet| tweet_collection << [tweet.created_at,user.username,tweet.text]}
+    end
+    tweet_collection
+  end
+end
