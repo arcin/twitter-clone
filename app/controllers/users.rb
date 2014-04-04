2706e5fd3708ac886e42829b7b11f54394f57e69
Charles Arciniega <charlesarcin@gmail.com>
2014-04-04 00:19:28 -0700

    God Commit

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -12,6 +12,14 @@ get '/public/:user' do
   erb :public_profile
 end
 
+post '/follow/:username' do
+  @user = User.find_by_username(params[:username])
+  @follower = User.find(session[:user_id])
+
+  Follower.create(user_id: @user.id, follower_id: @follower.id)
+  redirect "/#{@follower.username}"
+end
+
 post '/public_profile' do
   @users = params[:username]
   redirect "/public/#{@users}"
