b5b2292682ff4ddb4c742278c2b5cc8ae3038670
Charles Arciniega & Greg Gerla <pair+arcin.gregorygerla@devbootcamp.com>
2014-04-04 13:55:20 -0700

    greg left. I dont know

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -5,3 +5,10 @@ post '/tweet' do
 
   redirect "/public/#{@user.username}"
 end
+
+
+get '/retweet/:tweet_id' do
+  @original_tweeter = Tweet.find_by_text(params[:tweet_id]).user_id
+  Retweet.create(tweet_id: @original_tweeter, retweeter_id: session[:user_id])
+  redirect "/"
+end
