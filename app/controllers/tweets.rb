1f9872113e8f35cb16ab84f0fa25dc3f687ad751
Charles Arciniega <charlesarcin@gmail.com>
2014-04-04 17:20:39 -0700

    fixed some stuff

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -11,5 +11,5 @@ get '/retweet/:tweet_id' do
   @tweet = Tweet.find(params[:tweet_id])
   # @original_tweeter = User.find(@tweet.user_id)
   Retweet.create(tweet_id: @tweet.id, retweeter_id: session[:user_id])
-  redirect "/"
+  redirect "/#{User.find(session[:user_id]).username}"
 end
